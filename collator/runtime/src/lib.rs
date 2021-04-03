3c72aa7b9f6bb96b9499ddc008e2b63c43d8211f
jasl <jasl9187@hotmail.com>
2021-04-04 04:32:05 +0800

    follow latest Cumulus

--- collator/runtime/src/lib.rs
+++ collator/runtime/src/lib.rs
@@ -110,7 +110,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("phala-collator"),
 	impl_name: create_runtime_str!("phala-collator"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 2,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
