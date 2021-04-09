b14d9eb57ef4f5da2af7d0a97d3ef1a8790b53c9
jasl <jasl9187@hotmail.com>
2021-04-10 01:45:07 +0800

    follow latest Cumulus

--- collator/src/cli.rs
+++ collator/src/cli.rs
@@ -64,7 +64,7 @@ pub struct ExportGenesisStateCommand {
 	pub output: Option<PathBuf>,
 
 	/// Id of the parachain this state is for.
-	#[structopt(long, default_value = "30")]
+	#[structopt(long, default_value = "30", conflicts_with = "chain")]
 	pub parachain_id: u32,
 
 	/// Write output in binary. Default is to write in hex.
@@ -72,7 +72,7 @@ pub struct ExportGenesisStateCommand {
 	pub raw: bool,
 
 	/// The name of the chain for that the genesis state should be exported.
-	#[structopt(long)]
+	#[structopt(long, conflicts_with = "parachain-id")]
 	pub chain: Option<String>,
 }
 
